89fe1a33076cbde4059b0595fe673edbf753c7e3
JozySouza <josiane.fasou@gmail.com>
2022-07-14 15:40:58 -0300

    criando class Calculadora

--- primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,6 +8,8 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        String nome;
+        int numPaginas;
         Livro livro1 = new Livro(nome:"o problema dos 3 corpos", numPaginas:300);
         System.out.println(livro1);
 
